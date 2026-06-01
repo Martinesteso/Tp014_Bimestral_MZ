108bc1def76fd4a1976393d8ee1804e63ec9c0af
Zito3009 <riverzito09@gmail.com>
2026-06-01 14:12:47 -0300

    Progreso

---
+++ Assets/Scripts/UIMANAGER.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class UIMANAGER : MonoBehaviour
+{
+    public TextMeshProUGUI Txt_puntaje;
+    public TextMeshProUGUI Txt_tiempo;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
